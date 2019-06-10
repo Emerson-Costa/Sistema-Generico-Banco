4f4e4d9cf90793250562c5cf8a8ff083568fc0ea
Emerson Costa <vmaster204@gmail.com>
2019-06-10 13:10:23 -0400

    Ultima atualizacao valida
    Arrumando os metodos

--- src/classes/Agencia.java
+++ src/classes/Agencia.java
@@ -11,13 +11,41 @@ public class Agencia {
   private Endereco endereco;
   private String nomeAgencia;
   private String numeroAgencia; 
-
+    /*2*/
     public Agencia(String nomeAgencia, String numeroAgencia, String pais, String cidade, String enderecoAgencia, String bairro, String cep, int numero) {
         this.nomeAgencia = nomeAgencia;
         this.numeroAgencia = numeroAgencia;
         endereco = new Endereco(pais, cidade, enderecoAgencia, bairro, cep, numero);  
     }
-
+    /*3*/
+    public boolean abrirConta(String tipoConta, String cpf, String nomeCliente, String rua, String cep ,String numero, String dataNasc, String tipoCliente, String agencia, float valor){
+        return true;
+    }
+    /*4*/
+    public float saque(String numeroAgencia, String conta){
+        return 0;
+    }
+    /*5*/
+    public boolean depositar(String numeroAgencia, float valor){
+        return true;
+    }
+    /*6*/
+    public boolean transferencia(String agenciaOrigem, String contaOrigem, String agenciaDestino, String contaDestino ){
+        return true;
+    }
+    /*7*/
+    public boolean solicitaEmprestimo(String numeroAgencia, String conta, float valor){
+        return true;
+    }
+    /*8*/
+    public String gerarExtratos(String numeroAgencia, String conta){
+        return "";
+    }
+    /*9*/
+    public String gerarRelatorios(){
+        return "";
+    }
+    
     public String getNomeAgencia() {
         return nomeAgencia;
     }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,25 +6,33 @@ import java.util.Scanner;
 
 
 public class Main {
-
     
-    public static void main(String[] args) {
+    public static void main(String[] args) {  
         Agencia agencia;
         Boolean contaAberta = false;
         Scanner ler = new Scanner(System.in);
         
         int op = 0;
         while(op != 10){
+           System.out.print("Informe a Opcao: ");
            op  = ler.nextInt(); ler.nextLine();
            switch(op){
                case 1:
+                   System.out.print("Nome Agencia: ");
                    String nomeAgencia     = ler.nextLine();
+                   System.out.print("Numero Agencia: ");
                    String numeroAgencia   = ler.nextLine();
+                   System.out.print("País: ");
                    String pais            = ler.nextLine();
+                   System.out.print("Cidade: ");
                    String cidade          = ler.nextLine();
+                   System.out.print("Endereco: ");
                    String enderecoAgencia = ler.nextLine();
+                   System.out.print("Bairro: ");
                    String bairro          = ler.nextLine();
+                   System.out.print("CEP: ");
                    String cep             = ler.nextLine();
+                   System.out.print("Numero: ");
                    int  numero            = ler.nextInt(); 
                    
                    agencia                = new Agencia(nomeAgencia,numeroAgencia,pais,cidade,enderecoAgencia,bairro,cep,numero);
@@ -36,7 +44,8 @@ public class Main {
                case 2:
     
                    if(contaAberta){
-                       
+                     
+                     
                    }else{
                        System.out.println("Nenhuma conta ou agência foi aberta!");
                    }
